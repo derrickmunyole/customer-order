259dbd19791f06abe98d61ceade13acf4699fa61
derrickmunyole <derrickjust@outlook.com>
2024-11-17 19:01:42 +0300

    feature: Updated signal tests for customer creation

--- app/customers/tests/test_signals.py
+++ app/customers/tests/test_signals.py
@@ -1,34 +1,90 @@
 from django.test import TestCase
-from users.models import User
+from django.contrib.auth import get_user_model
 from customers.models import Customer
+from allauth.socialaccount.models import SocialAccount
 
 
 class CustomerSignalTest(TestCase):
-    # creating new user
-    def customer_entry_on_user_save(self):
+    def test_customer_creation_with_social_account(self):
+        # Create a user using get_user_model
+        User = get_user_model()
         user = User.objects.create(
             email="johnnytest@example.com",
-            oidc_id="test123"
+            username="johnnytest"
         )
 
-        # check if customer is automatically created
+        # Create a mock social account
+        social_account = SocialAccount.objects.create(
+            user=user,
+            provider='google',
+            extra_data={
+                'name': 'Johnny Test'
+            }
+        )
 
+        # Simulate the user_signed_up signal
+        from allauth.account.signals import user_signed_up
+        user_signed_up.send(
+            sender=User,
+            request=None,
+            user=user
+        )
+
+        # Check if customer is automatically created
         try:
             customer = Customer.objects.get(user=user)
             self.assertIsInstance(customer, Customer)
             self.assertEqual(customer.user, user)
-            self.assertEqual(customer.name, '')
-            self.assertEqual(customer.code, '')
-            self.assertEqual(customer.Phone, '')
+            self.assertEqual(customer.name, 'Johnny Test')
         except Customer.DoesNotExist:
-            self.fail("Customer wasn't created during user save")
+            self.fail("Customer wasn't created during user sign up")
 
-    def test_customer_is_unique_per_user_entry(self):
-        # create a new user verifying only one customer entry corresponding to
-        # created user
+    def test_customer_creation_without_social_account(self):
+        # Create a user using get_user_model
+        User = get_user_model()
         user = User.objects.create(
             email="johnnytest@example.com",
-            oidc_id="test123"
+            username="johnnytest"
+        )
+
+        # Simulate the user_signed_up signal
+        from allauth.account.signals import user_signed_up
+        user_signed_up.send(
+            sender=User,
+            request=None,
+            user=user
+        )
+
+        # Check if customer is created with fallback username
+        try:
+            customer = Customer.objects.get(user=user)
+            self.assertIsInstance(customer, Customer)
+            self.assertEqual(customer.user, user)
+            self.assertEqual(customer.name, 'johnnytest')
+        except Customer.DoesNotExist:
+            self.fail("Customer wasn't created during user sign up")
+
+    def test_customer_is_unique_per_user_signup(self):
+        # Create a user using get_user_model
+        User = get_user_model()
+        user = User.objects.create(
+            email="johnnytest@example.com",
+            username="johnnytest"
+        )
+
+        # Simulate the user_signed_up signal
+        from allauth.account.signals import user_signed_up
+        user_signed_up.send(
+            sender=User,
+            request=None,
+            user=user
+        )
+
+        # Trigger the signal again to test uniqueness
+        user_signed_up.send(
+            sender=User,
+            request=None,
+            user=user
         )
 
         customer_count = Customer.objects.filter(user=user).count()
