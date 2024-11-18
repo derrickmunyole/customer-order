6ea14bdd8f3d5007f20d624d5874304951888eab
derrickmunyole <derrickjust@outlook.com>
2024-11-18 19:56:33 +0300

    feature: Updated phone field to a maxlength of 13 to accomodate international format

--- app/customers/models.py
+++ app/customers/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from uuid import uuid4
+from django.contrib.auth import get_user_model
 
 
 class Customer(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
-    user = models.OneToOneField('users.User', on_delete=models.CASCADE)
+    user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE)
     name = models.CharField(max_length=80, null=True)
-    phone = models.CharField(max_length=10, null=True)
+    phone = models.CharField(max_length=13, null=True)
     code = models.CharField(max_length=20, null=True)
 
     def __str__(self) -> str:
