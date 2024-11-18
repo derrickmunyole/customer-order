0897160fea16b569c3ee852f415ec8df209faff2
derrickmunyole <derrickjust@outlook.com>
2024-11-18 19:48:14 +0300

    feature: Updated views and routes for the core app

--- app/core/urls.py
+++ app/core/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', view=views.homepage, name='home'),
-    path('login', view=views.login_view, name='login'),
+    path('login', view=views.CustomLoginView.as_view(), name='login'),
     path("logout", view=views.logout_view, name='logout'),
     path('accounts/', include('allauth.urls')),
 ]
