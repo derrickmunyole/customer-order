4c2c82c87fc626ec0b7b6555a00b93887f7c1a8b
derrickmunyole <derrickjust@outlook.com>
2024-11-19 21:26:15 +0300

    feature: Added CustomLoginView class for login

--- app/core/views.py
+++ app/core/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import logout
-from django.contrib.auth.decorators import login_required
 from allauth.account.views import LoginView
 from django.urls import reverse
 from .data import ITEM_LIST
