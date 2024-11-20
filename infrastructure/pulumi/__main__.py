1884646e7d692729e479b911711466ed26579104
derrickmunyole <derrickjust@outlook.com>
2024-11-20 09:47:29 +0300

    update: updated pulumi infrastructure config

--- infrastructure/pulumi/__main__.py
+++ infrastructure/pulumi/__main__.py
@@ -8,7 +8,7 @@ vpc = aws.ec2.Vpc(
     cidr_block="10.0.0.0/16",
     enable_dns_hostnames=True,
     enable_dns_support=True,
-    tags={"Name": "my-vpc"}
+    tags={"Name": "foreverLc-vpc"}
 )
 
 subnet = aws.ec2.Subnet(
@@ -17,7 +17,7 @@ subnet = aws.ec2.Subnet(
     cidr_block="10.0.1.0/24",
     availability_zone="us-east-1a",
     map_public_ip_on_launch=True,
-    tags={"Name": "my-subnet"}
+    tags={"Name": "forever-locket"}
 )
 
 # Creating Security group
@@ -56,7 +56,7 @@ instance = aws.ec2.Instance(
     vpc_security_group_ids=[security_group.id],
     subnet_id=subnet.id,
     ami="ami-0664c8f94c2a2261b",
-    tags={"Name": "web-server"}
+    tags={"Name": "foreverLc-server"}
 )
 
 # Export the instance's public IP
